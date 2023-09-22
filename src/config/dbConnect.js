1a86b79e63ac1752aff1cb7eb72e3eb45ed8bce9
setval42 <144036344+setval42@users.noreply.github.com>
2023-09-22 08:43:50 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
     mongoose.connect(
-      "mongodb+srv://labuser:Afc.123@cluster0.fwpzito.mongodb.net/livraria?retryWrites=true&w=majority"
+      "mongodb+srv://labuser:<password>@cluster0.fwpzito.mongodb.net/livraria?retryWrites=true&w=majority"
     );
     
     return mongoose.connection;
